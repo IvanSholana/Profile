ada8f0a9351dd7ed4c69a734235ca05a65f21e4d
Ivan Sholana <94579033+IvanSholana@users.noreply.github.com>
2023-04-23 19:56:45 +0700

    5 commit

--- dist/js/JQ.js
+++ dist/js/JQ.js
@@ -1,3 +1,14 @@
+$(window).scroll(function() {
+    var position = $('header').offset().top;
+    if (window.pageYOffset > position) {
+        $("header").addClass("navbar-fixed");
+    } else {
+        $("header").removeClass("navbar-fixed");
+    }
+});
+
+  
+
 $("#hamburger").click(function() { 
     $(this).toggleClass("hamburger-active");
 });
